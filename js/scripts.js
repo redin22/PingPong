332dab2bd5644ee736391452295ad3712db893fc
Rehan Edin <rehanedin@gmail.com>
2018-05-04 16:10:12 +0300

    add copyright details

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,36 @@
-//Business logic//
-var countUp=function(pong){
-  var result=[];
-  for(var i=1;i<=pong; i++){
-    var index= result.indexOf(i);
-    if(i%15===0){
-      result.splice(index,0);
-      result.push("pingpong");
-    }else if(i%5=== 0){
-      result.splice(index,0);
-      result.push("pong");
-    }else if (i%3 === 0);{
-      result.splice(index,0);
-      result.push("ping");
-    }else{
-      result.push(i);
-    }
-  }
-  return result;
-};
-
-
-//User Interface logic//
-
+//User Interface Logic
 $(document).ready(function(){
-  $("#ping-pong").submit(function(event){
-  $("results").empty();
-  var pong=parseInt($("input#pong").val());
-  var results=countUp(pong);
+ $("#userInput").submit(function(event){
+   $("#final").empty();
+   var countTo = parseInt($("input#countTo").val());
+   var results = countUp(countTo);
 
-  results.forEach(function(item){
-    $("#results").append('<li>'+item+'</li>')
-  });
-    event.preventDefault();
+   results.forEach(function(item){
+     $("#results").append('<li>' + item + '</li>');
+   });
+   event.preventDefault();
+ });
 });
-});
+
+
+//Business Logic
+var countUp = function(countTo){
+ var result = [];
+ for(var i = 1; i <= countTo; i++){
+     var index = result.indexOf(i);
+     if(i % 15 === 0) {
+       result.splice(index, 0);
+       result.push('pingpong');
+     }else if(i % 5 === 0) {
+       result.splice(index, 0);
+       result.push('pong');
+     }else if(i % 3 === 0) {
+       result.splice(index, 0);
+       result.push('ping');
+     }else{
+       result.push(i);
+     }
+ }
+
+ return result;
+};
