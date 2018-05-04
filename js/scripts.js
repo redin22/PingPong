cda24477c84d26d49086a3b207773d08b7c6c595
Rehan Edin <rehanedin@gmail.com>
2018-05-04 09:19:48 +0300

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-  $("ping-pong").submit(function(event){
+  $("#userInput").submit(function(event){
   event.preventDefault();
+  $("output").empty();
+  var countTo=parseInt($("input#countTo").val());
+  var output=countUp(countTo)
 });
 var numbers=[1,2,3,4,5,6,7,8,9];
 alert(Enter a number);
