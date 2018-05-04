9aa78a6ae6cb6373dbb25e90fecaa3efd329663e
Rehan Edin <rehanedin@gmail.com>
2018-05-04 10:43:56 +0300

    add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,36 @@
+//Business logic//
+var countUp=function(pong){
+  var result=[];
+  for(var i=1;i<=pong; i++){
+    var index= result.indexOf(i);
+    if(i%15===0){
+      result.splice(index,0);
+      result.push("pingpong");
+    }else if(i%5=== 0){
+      result.splice(index,0);
+      result.push("pong");
+    }else if (i%3 === 0);{
+      result.splice(index,0);
+      result.push("ping");
+    }else{
+      result.push(i);
+    }
+  }
+  return result;
+};
+
+
+//User Interface logic//
+
 $(document).ready(function(){
   $("#ping-pong").submit(function(event){
-  event.preventDefault();
   $("results").empty();
   var pong=parseInt($("input#pong").val());
-  var results=countUp(pong)
-});
+  var results=countUp(pong);
 
-alert(Enter a number);
+  results.forEach(function(item){
+    $("#results").append('<li>'+item+'</li>')
+  });
+    event.preventDefault();
+});
+});
