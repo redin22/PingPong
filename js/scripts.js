7c92cabb21f7023ae53588d14f3216bee0d4dfc9
Rehan Edin <rehanedin@gmail.com>
2018-05-04 10:04:41 +0300

    add inline display for the buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $("#userInput").submit(function(event){
+  $("#ping-pong").submit(function(event){
   event.preventDefault();
-  $("output").empty();
-  var countTo=parseInt($("input#countTo").val());
-  var output=countUp(countTo)
+  $("results").empty();
+  var pong=parseInt($("input#pong").val());
+  var results=countUp(pong)
 });
-var numbers=[1,2,3,4,5,6,7,8,9];
+
 alert(Enter a number);
